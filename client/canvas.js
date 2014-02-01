d313ff02e29404821f60849573bf245bb31b2b76
Dom Armstrong <dom@dom-armstrong.co.uk>
2014-02-01 16:29:06 +0000

    add events to canvas

--- client/canvas.js
+++ client/canvas.js
@@ -1,13 +1,40 @@
-function Canvas() {
-    this.canvas = document.getElementById('canvas');
-    this.context = this.canvas.getContext('2d');
+function Canvas(id) {
+    this.c = document.getElementById(id);
+    this.cx = this.c.getContext('2d');
+    this.events = {};
 }
 Canvas.prototype = {
     draw: function draw() {
 
     },
     clear: function clear() {
-        this.canvas.width = this.canvas.width;
+        this.cx.clearRect(0, 0, this.c.width, this.c.height);
+    },
+    on: function (name, fn) {
+        // TODO: make this work for namespaces and multiple fns array
+        this.off(name, fn);
+        this.c.addEventListener(name, fn);
+        this.events[name] = fn;
+    },
+    off: function (name, fn) {
+        // TODO: make this work for namespaces and multiple fns array
+        if (typeof name == 'string' && !fn) {
+            fn = this.events.name; 
+        } else if (typeof name == 'function') {
+            fn = name;
+            name = null;
+            for (var n in this.events) {
+                if (this.events.hasOwnProperty(n)) {
+                    if (fn == this.events[n]) {
+                        name = n;
+                    }
+                }
+            }
+            if (!name) {
+                throw new Error('Cannot find event handler');
+            }
+        }
+        this.c.removeEventListener(name, fn);
     }
 };
 module.exports = Canvas;
