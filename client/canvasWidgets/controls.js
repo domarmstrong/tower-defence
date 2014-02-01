7cf1596ea4194f6809bec2a30f9449645abf87c8
Dom Armstrong <dom@dom-armstrong.co.uk>
2014-02-01 16:29:47 +0000

    inherit button from controls and use this.state

--- client/canvasWidgets/controls.js
+++ client/canvasWidgets/controls.js
@@ -1,36 +1,56 @@
+var util = require('../../common/util');
+var Mouse = require('../mouse');
+var mouse = new Mouse();
+
+function Control() {
+}
+Control.prototype = {
+    set: function set(obj) {
+        this.state = util.extend(this.state, obj);
+        this.screen.draw();
+    }
+};
+
+
 function Button(props) {
     this.props = props;
+    this.state = util.extend({}, this.defaults);
+    this.state = util.extend(this.state, this.props);
+    this.state.shape = {
+        x: props.x, 
+        y: props.y, 
+        w: props.w, 
+        h: props.h 
+    };
 }
-Button.prototype = {
+Button.prototype = util.inherit(Control, {
+    shape: 'rect',
+    defaults: {
+        background: '#222555',
+        color: '#FFFFFF',
+    },
     draw: function draw(c) {
-        var p = this.props;
-        c.fillStyle = '#222555';
-        c.fillRect(p.x, p.y, p.w, p.h);
+        var _ = this.state.shape;
+        c.fillStyle = this.state.background;
+        c.fillRect(_.x, _.y, _.w, _.h);
         this.text(c);
     },
     text: function text(c) {
         var p = this.props;
+        var _ = this.state.shape;
         if (! p.text) return;
         c.font = 'bold 18px sans-serif';
         c.textAlign = 'center';
         c.textBaseline = 'middle';
-        c.fillStyle = '#FFFFFF';
-        c.fillText(p.text, p.x + p.w / 2, p.y + p.h / 2)
+        c.fillStyle = this.state.color;
+        c.fillText(p.text, _.x + _.w / 2, _.y + _.h / 2)
     },
     click: function click(event) {
-        var p = this.props,
-            x = event.offsetX,
-            y = event.offsetY;
-        if (
-            (x > p.x && x < p.x + p.w) &&
-            (y > p.y && y < p.y + p.h)
-        ) {
-            if (this.props.click) {
-                this.props.click();
-            }
+        if (this.props.click) {
+            this.props.click.call(this);
         }
     }
-};
+});
 module.exports = {
     'Button': Button
 };
